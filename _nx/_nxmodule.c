45457fd1fda1fc450485c49d37d9d1c4dc972c59
DavidBuchanan314 <d@vidbuchanan.co.uk>
2018-03-23 02:14:17 +0000

    Fix u128 truncation, implement fs functions for mounting saves

--- _nx/_nxmodule.c
+++ _nx/_nxmodule.c
@@ -1,4 +1,5 @@
 #include <switch.h>
+#include <limits.h>
 
 #include "Python.h"
 
@@ -9,6 +10,53 @@
 
 PyDoc_STRVAR(module_doc,
 "The _nx module is a thin wrapper around libnx functions\n");
+
+/* internal utils */
+
+static PyObject *
+_PyLong_FromU128(u128 int_in)
+{
+    unsigned long high, low;
+    PyObject *result, *result_low, *sixty_four;
+
+    high = int_in >> 64;
+    low = int_in;
+
+    result = PyLong_FromUnsignedLong(high);
+    result_low = PyLong_FromUnsignedLong(low);
+    sixty_four = PyLong_FromUnsignedLong(64);
+    result = PyNumber_InPlaceLshift(result, sixty_four);
+    result = PyNumber_InPlaceOr(result, result_low);
+
+    Py_DECREF(result_low);
+    Py_DECREF(sixty_four);
+
+    return result;
+}
+
+static int
+_PyLongToU128Converter(PyObject *pylong_in, u128 *int_out)
+{
+    u128 result;
+    PyObject *in_high, *sixty_four;
+
+    // XXX: add type checking!!!
+
+    sixty_four = PyLong_FromUnsignedLong(64);
+    in_high = PyNumber_Rshift(pylong_in, sixty_four);
+
+    result = PyLong_AsUnsignedLongMask(in_high);
+    result <<= 64;
+    result |= PyLong_AsUnsignedLongMask(pylong_in);
+
+    Py_DECREF(in_high);
+    Py_DECREF(sixty_four);
+
+    *int_out = result;
+    return 1;
+}
+
+/* end internal utils */
 
 /* hid */
 
@@ -62,25 +110,58 @@ static PyObject *
 _nx_account_get_active_user(PyObject *self, PyObject *args)
 {
     Result rc;
-    unsigned long long user_id;
+    u128 user_id;
     bool account_selected;
 
-    rc = accountGetActiveUser((u128*) &user_id, &account_selected);
+    rc = accountGetActiveUser(&user_id, &account_selected);
     NX_ASSERT_IPC_OK(rc);
 
     if (!account_selected)
         Py_RETURN_NONE;
 
-    return PyLong_FromUnsignedLongLong(user_id);
+    return _PyLong_FromU128(user_id);
 }
 
 /* end account */
+
+/* fs */
+
+PyDoc_STRVAR(_nx_fs_mount_savedata_doc,
+    "fs_mount_savedata(mountpoint, title_id, user_id)"); // TODO
+
+static PyObject *
+_nx_fs_mount_savedata(PyObject *self, PyObject *args)
+{
+    const char *mountpoint;
+    unsigned long title_id;
+    u128 user_id;
+    FsFileSystem tmpfs;
+    Result rc;
+    int ret;
+
+    if (!PyArg_ParseTuple(args, "skO&", &mountpoint, &title_id, _PyLongToU128Converter, &user_id))
+        return NULL;
+
+    rc = fsMount_SaveData(&tmpfs, title_id, user_id);
+    NX_ASSERT_IPC_OK(rc);
+
+    ret = fsdevMountDevice(mountpoint, tmpfs);
+    if (ret == -1) {
+        PyErr_SetString(PyExc_OSError, "fsdevMountDevice() Failed");
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+/* end fs */
 
 static PyMethodDef module_methods[] = {
     {"hid_scan_input", _nx_hid_scan_input, METH_NOARGS, _nx_hid_scan_input_doc},
     {"hid_keys_down", _nx_hid_keys_down, METH_VARARGS, _nx_hid_keys_down_doc},
     {"account_initialize", _nx_account_initialize, METH_NOARGS, _nx_account_initialize_doc},
     {"account_get_active_user", _nx_account_get_active_user, METH_NOARGS, _nx_account_get_active_user_doc},
+    {"fs_mount_savedata", _nx_fs_mount_savedata, METH_VARARGS, _nx_fs_mount_savedata_doc},
     {NULL, NULL}
 };
 
